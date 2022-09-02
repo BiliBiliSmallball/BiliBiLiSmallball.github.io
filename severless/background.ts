29fa7b2c433371c9dbb10813f857aa47078b8f2b
dell <dell@gitee.com>
2022-09-02 22:02:21 +0800

    将源码换成中文的

--- severless/background.ts
+++ severless/background.ts
@@ -6,9 +6,13 @@ app.get("/test", async (res, req) => {
   req.send("数据成功返回");
 });
 
+app.post("/user", async (res, req) => {
+  console.log(res.body)
+});
+
 app.listen(20221, function () {
   //端口监听7700
-  console.log("监听端口: 8081 已经开放"); //console.log("app is listening at 127.0.0.1:8081 ");
+  console.log("监听端口: 20221 已经开放"); //console.log("app is listening at 127.0.0.1:8081 ");
   console.log("请打开 http://localhost:20221/ 访问结果");
 });
 //输入

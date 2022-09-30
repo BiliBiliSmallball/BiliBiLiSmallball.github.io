7014ab0c589f3f93a6e00824859524eca00a287a
dell <dell@gitee.com>
2022-09-30 11:36:26 +0800

    更改了留言板架构

--- hearing/hearing_find.js
+++ hearing/hearing_find.js
@@ -1,7 +1,9 @@
-//检测是否有本地账户
 
+//检测是否有本地账户
 let User_n = window.localStorage.getItem("u_name");
 if (!User_n) {
   alert("请注册后使用！");
 }
-window.document.getElementById("D_name").innerHTML = User_n;
+
+let hand = document.getElementById("title");
+hand.innerHTML = String(User_n.value); //ncaught TypeError:无法读取null的属性（读取“value”）

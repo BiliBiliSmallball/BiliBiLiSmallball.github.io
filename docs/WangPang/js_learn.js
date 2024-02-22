d5d569400382abcb23a2b4fa2bc6dd98c0a142c5
Ender_Zhu <zzy1535878824@163.com>
2024-02-22 19:59:38 +0800

    重构一下

---
+++ docs/WangPang/js_learn.js
@@ -0,0 +1,30 @@
+document.write(DocumentTimeline)
+alert("这是写入");
+console.log('写入数据')
+/* 
+alert   弹窗
+document.write   写入网页
+console.log('')   写入控制台
+注：编辑器中单行注释的快捷键为 ctrl + /
+注：编辑器中duo行注释的快捷键为 shift +alt+a 
+*/
+let name = 10// var name = 10;
+function test(name,init){
+  if (name === init) {
+    return name;
+  } else {
+    document.write("不是10哦");
+  }
+}
+/* let  ：创建常量
+*/
+/*
+数据类型
+String 字符串
+Number数
+Boolean 布尔
+null  空值
+undefined   未定义
+Object  对象
+ */
+/*https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects */
